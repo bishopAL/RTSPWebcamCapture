60cb8fc47dd0642bdc769f9200d7b5daa52ddf11
Wang Bingcheng <470797339@qq.com>
2020-10-20 11:28:25 +0800

    update page convert function

--- webcam_detection.py
+++ webcam_detection.py
@@ -10,21 +10,12 @@ class WebcamApp:
         # Tkinter Initialization
         self.root = Tk()
         self.root.title('Webcam')
-        self.movieLabel = Label(self.root)
-        self.movieLabel.pack(padx=10, pady=10)
-        self.playOrPauseButton = Button(master=self.root, text='暂停/播放', command=self.pp_convert)
-        self.playOrPauseButton.pack()
-        self.captureVideoButton = Button(master=self.root, text='开始/停止录像', command=self.save_video)
-        self.captureVideoButton.pack()
-        self.captureButton = Button(master=self.root, text='截图', command=self.save_pic)
-        self.captureButton.pack()
-        self.quit_button = Button(master = self.root, text='退出', command=self.quit)
-        self.quit_button.pack()
+        self.mainPage_init()
         self.date = str(time.localtime(time.time()).tm_year) + '-' + str(time.localtime(time.time()).tm_mon) + '-' \
                     + str(time.localtime(time.time()).tm_mday) + ' ' + str(time.localtime(time.time()).tm_hour) + ':' \
                     + str(time.localtime(time.time()).tm_min) + ':' + str(time.localtime(time.time()).tm_sec)
         self.playOrPause = True  # True: play; False: pause
-        self.GO_FLAG = False
+        self.updating_flag = False
         self.saveVideoFlag = False
         self.quit_flag = False
 
@@ -37,12 +28,55 @@ class WebcamApp:
         t0 = Thread(target=self.webcam_read)
         t0.setDaemon(True)
         t0.start()
+        t1 = Thread(target=self.updating_frame)
+        t1.setDaemon(True)
+        t1.start()
 
-    def webcam_read(self):
-        while 1:
-            if self.playOrPause:
-                ret, self.frame = self.cap.read()
-                self.GO_FLAG = True
+    def mainPage_init(self):
+        self.mainPage = Frame(self.root, )
+        self.mainPage.pack()
+        self.movieLabel = Label(self.mainPage)
+        self.movieLabel.pack(padx=10, pady=10)
+        self.examine = Button(master=self.mainPage, text='查看', command=self.examine_init)
+        self.examine.pack()
+        self.project = Button(master=self.mainPage, text='项目', command=self.project_init)
+        self.project.pack()
+        self.captureVideoButton = Button(master=self.mainPage, text='开始录像', command=self.save_video)
+        self.captureVideoButton.pack()
+        self.captureVideoButton = Button(master=self.mainPage, text='停止录像', command=self.stop_video)
+        self.captureVideoButton.pack()
+        self.captureButton = Button(master=self.mainPage, text='截图', command=self.save_pic)
+        self.captureButton.pack()
+        self.quit_button = Button(master=self.mainPage, text='退出', command=self.quit)
+        self.quit_button.pack()
+
+    def examine_init(self):
+        self.playOrPause = False
+        self.updating_flag = False
+        self.mainPage.destroy()
+        self.examinePage = Frame(self.root, )
+        self.examinePage.pack()
+        btn_back = Button(self.examinePage, text='查看返回', command=self.examinePage2mainPage)
+        btn_back.pack()
+
+    def examinePage2mainPage(self):
+        self.examinePage.destroy()
+        self.mainPage_init()
+        self.playOrPause = True
+
+    def project_init(self):
+        self.playOrPause = False
+        self.updating_flag = False
+        self.mainPage.destroy()
+        self.projectPage = Frame(self.root, )
+        self.projectPage.pack()
+        btn_back = Button(self.projectPage, text='项目返回', command=self.projectPage2mainPage)
+        btn_back.pack()
+
+    def projectPage2mainPage(self):
+        self.projectPage.destroy()
+        self.mainPage_init()
+        self.playOrPause = True
 
     def pp_convert(self):
         if self.playOrPause:  # play -> pause
@@ -62,8 +96,14 @@ class WebcamApp:
             self.video = cv2.VideoWriter(self.date + '.avi', fourcc, 20.0, (self.width, self.height), True)
             self.saveVideoFlag = not self.saveVideoFlag
         else:
+            pass
+
+    def stop_video(self):
+        if self.saveVideoFlag:
             self.video.release()
             self.saveVideoFlag = not self.saveVideoFlag
+        else:
+            print("视频尚未开始录制，请先点击开始录像。")
 
     def save_pic(self):
         self.date = str(time.localtime(time.time()).tm_year) + '-' + str(time.localtime(time.time()).tm_mon) + '-' \
@@ -76,17 +116,28 @@ class WebcamApp:
         self.quit_flag = True
         self.root.destroy()
 
+    def webcam_read(self):
+        while not self.quit_flag:
+            if self.playOrPause:
+                ret, temp_frame = self.cap.read()
+                if ret:
+                    self.frame = temp_frame
+                    self.updating_flag = True
+
+    def updating_frame(self):
+        while not self.quit_flag:
+            if self.updating_flag and self.playOrPause:
+                if self.saveVideoFlag:
+                    self.video.write(self.frame)
+                img = cv2.cvtColor(self.frame, cv2.COLOR_BGR2RGBA)
+                current_image = Image.fromarray(img).resize((540, 320))
+                imgtk = ImageTk.PhotoImage(image=current_image)
+                self.movieLabel.imgtk = imgtk
+                self.movieLabel.config(image=imgtk)
+                self.movieLabel.update()
+
 
 webcam = WebcamApp(0)
 # webcam = WebcamApp('rtsp://admin:admin@192.168.10.183/11')
+webcam.root.mainloop()
 
-while not webcam.quit_flag:
-    if webcam.GO_FLAG:
-        if webcam.saveVideoFlag:
-            webcam.video.write(webcam.frame)
-        img = cv2.cvtColor(webcam.frame, cv2.COLOR_BGR2RGBA)
-        current_image = Image.fromarray(img).resize((540, 320))
-        imgtk = ImageTk.PhotoImage(image=current_image)
-        webcam.movieLabel.imgtk = imgtk
-        webcam.movieLabel.config(image=imgtk)
-        webcam.movieLabel.update()
